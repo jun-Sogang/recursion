627e484f3193dbb6eb2baf4dda047f07f954d8fd
Vinnie Giarrusso <vin.giarrusso@gmail.com>
2013-10-27 22:26:36 -0700

    passing all tests, extra credit escaping not completed

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,5 +3,38 @@
 
 // but you don't so you're going to have to write it from scratch:
 var stringifyJSON = function (obj) {
-  // your code goes here
+  if (typeof obj === "number") {
+  	return String(obj);
+  }
+  if (obj === null) {
+  	return String(obj);
+  }
+  if (typeof obj === "boolean"){
+  	return String(obj);
+  }
+  if (typeof obj === "string") {
+  	return '"' + obj + '"';
+  }
+ if (Array.isArray(obj)) {
+ 	//I chose to use an array and not a string to store our stringified pieces because
+ 	//if we use a string, we'll have to manually add the commas separating elements, which 
+ 	//also means we'll need to take off the trailing comma from the end.  We can avoid this by using .join instead.
+ 	var holding = [];
+ 	for (var i = 0; i < obj.length; i++){
+ 		holding.push(stringifyJSON(obj[i]));
+ 	}
+ 	return '[' + holding.join(",") + ']';
+ }
+ if (typeof obj === "object") {
+ 	var holding = [];
+ 	for(var prop in obj) {
+ 		if (typeof obj[prop] === "function" || undefined) {
+ 			return '{}';
+ 		}
+ 		else {
+ 		holding.push(stringifyJSON(prop) + ":" + stringifyJSON(obj[prop]));
+ 		}
+ 	}
+ 	return '{' + holding.join(",") + '}';
+ }
 };
