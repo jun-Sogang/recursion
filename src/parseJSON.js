00a91768056076d3495c79c5dba0a2ff0c951279
Vinnie Giarrusso <vin.giarrusso@gmail.com>
2013-11-01 23:08:34 -0700

    start on parseJSON(), string and next functions working.

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -3,5 +3,28 @@
 
 // but you're not, so you'll write it from scratch:
 var parseJSON = function (json) {
-  // your code goes here
+  	
+  var index = 0;
+  var ca = json.charAt(index);
+
+  var next = function() {
+  	if (index < json.length - 1) {
+  		ca = json.charAt(index + 1)
+  		index += 1;
+  	}
+  	else {
+  		ca = undefined;
+  	}
+  };
+
+  	var string = function() {
+  		var result = '';
+  		while (ca != undefined) {
+  			result += ca;
+  			next();
+  		}
+  	};
+  };
+
+
 };
