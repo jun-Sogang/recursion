b534b212ea9205cf2852ef94520b04e74e1741be
Hack Reactor Students <students@hackreactor.com>
2014-01-06 15:24:43 -0800

    completed numbers and strings

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,38 +3,11 @@
 
 // but you don't so you're going to have to write it from scratch:
 var stringifyJSON = function (obj) {
-  if (typeof obj === "number") {
-  	return String(obj);
+  if(typeof obj === "string"){
+    return '"' + obj + '"';
   }
-  if (obj === null) {
-  	return String(obj);
+  if(Array.isArray(obj)){
+    return "[]";
   }
-  if (typeof obj === "boolean"){
-  	return String(obj);
-  }
-  if (typeof obj === "string") {
-  	return '"' + obj + '"';
-  }
- if (Array.isArray(obj)) {
- 	//I chose to use an array and not a string to store our stringified pieces because
- 	//if we use a string, we'll have to manually add the commas separating elements, which 
- 	//also means we'll need to take off the trailing comma from the end.  We can avoid this by using .join instead.
- 	var holding = [];
- 	for (var i = 0; i < obj.length; i++){
- 		holding.push(stringifyJSON(obj[i]));
- 	}
- 	return '[' + holding.join(",") + ']';
- }
- if (typeof obj === "object") {
- 	var holding = [];
- 	for(var prop in obj) {
- 		if (typeof obj[prop] === "function" || undefined) {
- 			return '{}';
- 		}
- 		else {
- 		holding.push(stringifyJSON(prop) + ":" + stringifyJSON(obj[prop]));
- 		}
- 	}
- 	return '{' + holding.join(",") + '}';
- }
+  return "" + obj;
 };
