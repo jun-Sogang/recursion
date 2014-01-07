70c46fc76c3605bc501f6fface4d7b2ad09b32c3
Hack Reactor Students <students@hackreactor.com>
2014-01-06 16:07:09 -0800

    finished stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -6,10 +6,24 @@ var stringifyJSON = function (obj) {
   if(typeof obj === "string"){
     return '"' + obj + '"';
   }
+  if(obj === null){
+    return "" + obj;
+  }
   if(Array.isArray(obj)){
     return "["+ obj.map(function(item){
       return stringifyJSON(item);
     }).join(",") + "]";
+  }
+  if(typeof obj === "object") {
+    var results = [];
+    for (var prop in obj){
+      if(typeof obj[prop] === "function" || typeof obj[prop] === undefined) {
+        return "{}";
+      }else{
+       results.push(stringifyJSON(prop) + ":" + stringifyJSON(obj[prop]));
+     }
+    }
+    return "{" + results.join(",") + "}";
   }
   return "" + obj;
 
